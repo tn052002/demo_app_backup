8511866363173117f95bbbc4f4a4e524e5d0f933
trung nguyen <trung.nguyen@wizecommerce.com>
2012-08-28 16:56:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1c2f43a68052359ad21ea9965b2363a9940694f3003d2ce10b0500f99df89e21e8d219723b9993aafa6a390140f82e8ba0581edd009f35427e21e23ab19c9541'
